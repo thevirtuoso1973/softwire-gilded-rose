9adb4d24ade8ff7bafd7035271ed562ee0535ed4
Chris <Chris.Tomy@softwire.com>
2020-07-07 13:37:22 +0100

    fixed fixme and added trivial test

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -9,11 +9,20 @@ class GildedRoseTest {
         val items = arrayOf<Item>(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
-
+        assertEquals("foo", app.items[0].name)
     }
 
+    @Test fun trivial() {
+        val name = "bar"
+        val daysleft = 1
+        val initquality = 1
+        val items = arrayOf<Item>(Item(name, daysleft, initquality))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(name, app.items[0].name)
+        assertEquals(daysleft-1, app.items[0].sellIn)
+        assertEquals(initquality-1, app.items[0].quality)
+    }
 
 }
-
 
