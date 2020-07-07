ae8f478027f00150a1f71429bcc24a35c16253d3
Chris <Chris.Tomy@softwire.com>
2020-07-07 16:54:27 +0100

    improve more tests

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -18,7 +18,19 @@ import org.junit.Test
 
 
 class GildedRoseTest {
-    private val testItems = arrayOf(Item("foo", -1, 4))
+    private val testItems = arrayOf(
+            Item("foo", -1, 4),
+            Item("+5 Dexterity Vest", 10, 20), //
+            Item("Aged Brie", 2, 0), //
+            Item("Elixir of the Mongoose", 5, 7), //
+            Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            // this conjured item does not work properly yet
+            Item("Conjured Mana Cake", 3, 6)
+    )
 
     @Test fun foo() {
         val items = arrayOf<Item>(Item("foo", 0, 0))
@@ -48,7 +60,11 @@ class GildedRoseTest {
                 val initQuality = item.quality
                 val app = GildedRose(arrayOf(item), qualityRate)
                 app.updateQuality()
-                assertEquals(initQuality-(2*qualityRate), app.items[0].quality)
+                if (item.name.startsWith("Conjured")) {
+                    assertEquals(initQuality-(4*qualityRate), app.items[0].quality)
+                } else {
+                    assertEquals(initQuality-(2*qualityRate), app.items[0].quality)
+                }
             }
         }
     }
@@ -63,11 +79,19 @@ class GildedRoseTest {
     }
 
     @Test fun agedBrieQualityIncrease() {
-        val name = "Aged Brie"; val daysLeft = 2; val initQuality = 1
-        val items = arrayOf<Item>(Item(name, daysLeft, initQuality))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals(initQuality+1, app.items[0].quality)
+        val qualityRate = 1
+        for (item in testItems) {
+            if (item.name.startsWith("Aged Brie")) {
+                val initQuality = item.quality
+                val app = GildedRose(arrayOf(item), qualityRate)
+                app.updateQuality()
+                if (item.sellIn < 0) {
+                    assertEquals(initQuality+(2*qualityRate), app.items[0].quality)
+                } else {
+                    assertEquals(initQuality+qualityRate, app.items[0].quality)
+                }
+            }
+        }
     }
 
 }
