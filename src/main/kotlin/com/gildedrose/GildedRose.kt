53bd3fe298911c17d1566dfd2b36e644c95dd85a
shavi-jay <60151734+shavi-jay@users.noreply.github.com>
2020-07-07 17:05:06 +0100

    Replacing increases with rate (with the exception of tickets) and added the conjured item.

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,15 +1,20 @@
 package com.gildedrose
 
+import kotlin.concurrent.fixedRateTimer
+import kotlin.math.max
+
 class GildedRose(var items: Array<Item>, val rate: Int = 1) {
 
     fun updateQuality() {
         //Decreasing quality before SellIn date is passed
         for (i in items.indices) {
             when (items[i].name) {
-                "Sulfuras, Hand of Ragnaros" -> {items[i].quality = items[i].quality}
-                "Aged Brie" -> {if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-                }}
+                "Sulfuras, Hand of Ragnaros" -> {
+                    items[i].quality = items[i].quality
+                }
+                "Aged Brie" -> {
+                    items[i].quality = maxOf(items[i].quality + rate,50)
+                }
                 "Backstage passes to a TAFKAL80ETC concert" -> {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1
@@ -24,10 +29,17 @@ class GildedRose(var items: Array<Item>, val rate: Int = 1) {
                             items[i].quality = items[i].quality + 1
                         }
                     }
+                    //Note:
+                    //Rate of increase was not specified for other items, but it was for tickets
                 }
-                else -> {if (items[i].quality > 0) {
-                    items[i].quality = items[i].quality - 1
-                }}
+                "Conjured Mana Cake" -> {
+                    items[i].quality = minOf(items[i].quality - 2*rate,0)
+                }
+                else -> {
+                    items[i].quality = minOf(items[i].quality - rate,0)
+                }
+
+
             }
 
             // Decreasing SellIn date except for Sulfuras
@@ -38,12 +50,21 @@ class GildedRose(var items: Array<Item>, val rate: Int = 1) {
             //Extra decrease in quality after passing SellIn date
             if (items[i].sellIn < 0) {
                 when (items[i].name) {
-                    "Aged Brie" -> {items[i].quality = items[i].quality + 1}
-                    "Backstage passes to a TAFKAL80ETC concert" -> {items[i].quality = 0}
-                    "Sulfuras, Hand of Ragnaros" -> {items[i].quality = items[i].quality}
-                    else -> {if (items[i].quality > 0) {
-                        items[i].quality = items[i].quality - 1
-                    }}
+                    "Sulfuras, Hand of Ragnaros" -> {
+                        items[i].quality = items[i].quality
+                    }
+                    "Aged Brie" -> {
+                        items[i].quality = maxOf(items[i].quality + rate, 50)
+                    }
+                    "Backstage passes to a TAFKAL80ETC concert" -> {
+                        items[i].quality = 0
+                    }
+                    "Conjured Mana Cake" -> {
+                        items[i].quality = minOf(items[i].quality - 2 * rate, 0)
+                    }
+                    else -> {
+                        items[i].quality = minOf(items[i].quality - rate, 0)
+                    }
                 }
             }
         }
