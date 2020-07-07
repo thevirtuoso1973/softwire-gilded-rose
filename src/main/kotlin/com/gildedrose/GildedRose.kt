a28a1a961c5490930055d3a1694377e5b945fb52
Chris <Chris.Tomy@softwire.com>
2020-07-07 17:47:14 +0100

    change condition to check prefix instead of exact string

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,14 +5,14 @@ class GildedRose(var items: Array<Item>, private val rate: Int = 1) {
     fun updateQuality() {
         //Decreasing quality before SellIn date is passed
         for (i in items.indices) {
-            when (items[i].name) {
-                "Sulfuras, Hand of Ragnaros" -> {
+            when {
+                items[i].name.startsWith("Sulfuras") -> {
                     items[i].quality = items[i].quality
                 }
-                "Aged Brie" -> {
+                items[i].name.startsWith("Aged Brie") -> {
                     items[i].quality = minOf(items[i].quality + rate,50)
                 }
-                "Backstage passes to a TAFKAL80ETC concert" -> {
+                items[i].name.startsWith("Backstage passes") -> {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1
                     }
@@ -29,7 +29,7 @@ class GildedRose(var items: Array<Item>, private val rate: Int = 1) {
                     //Note:
                     //Rate of increase was not specified for other items, but it was for tickets
                 }
-                "Conjured Mana Cake" -> {
+                items[i].name.startsWith("Conjured") -> {
                     items[i].quality = maxOf(items[i].quality - 2*rate,0)
                 }
                 else -> {
@@ -40,23 +40,23 @@ class GildedRose(var items: Array<Item>, private val rate: Int = 1) {
             }
 
             // Decreasing SellIn date except for Sulfuras
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (!items[i].name.startsWith("Sulfuras")) {
                 items[i].sellIn = items[i].sellIn - 1
             }
 
             //Extra decrease in quality after passing SellIn date
             if (items[i].sellIn < 0) {
-                when (items[i].name) {
-                    "Sulfuras, Hand of Ragnaros" -> {
+                when {
+                    items[i].name.startsWith("Sulfuras") -> {
                         items[i].quality = items[i].quality
                     }
-                    "Aged Brie" -> {
+                    items[i].name.startsWith("Aged Brie") -> {
                         items[i].quality = minOf(items[i].quality + rate, 50)
                     }
-                    "Backstage passes to a TAFKAL80ETC concert" -> {
+                    items[i].name.startsWith("Backstage passes") -> {
                         items[i].quality = 0
                     }
-                    "Conjured Mana Cake" -> {
+                    items[i].name.startsWith("Conjured") -> {
                         items[i].quality = maxOf(items[i].quality - 2 * rate, 0)
                     }
                     else -> {
