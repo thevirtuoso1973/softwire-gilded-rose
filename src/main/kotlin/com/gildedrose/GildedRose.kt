1c920afcaec367762f75052c59d643290fe7d6ea
shavi-jay <60151734+shavi-jay@users.noreply.github.com>
2020-07-07 16:00:14 +0100

    General decrease in quality: replacing if chain with a when

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,30 +5,29 @@ class GildedRose(var items: Array<Item>) {
     fun updateQuality() {
         //Decreasing quality before SellIn date is passed
         for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
+            when (items[i].name) {
+                "Sulfuras, Hand of Ragnaros" -> {items[i].quality = items[i].quality}
+                "Aged Brie" -> {if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
+                }}
+                "Backstage passes to a TAFKAL80ETC concert" -> {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1
+                    }
+                    if (items[i].sellIn < 11) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1
                         }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
+                    }
+                    if (items[i].sellIn < 6) {
+                        if (items[i].quality < 50) {
+                            items[i].quality = items[i].quality + 1
                         }
                     }
                 }
+                else -> {if (items[i].quality > 0) {
+                    items[i].quality = items[i].quality - 1
+                }}
             }
 
             // Decreasing SellIn date except for Sulfuras
